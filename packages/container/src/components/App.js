e59e4aba7975d5bd5923152c2d617ab99417da4a
mohammad-23 <mohammad8758@hotmail.com>
2021-06-24 21:18:29 +0530

    Update Container Header and add Cloudfront provider

--- packages/container/src/components/App.js
+++ packages/container/src/components/App.js
@@ -5,7 +5,7 @@ import MarketingApp from "./MarketingApp";
 const App = () => {
   return (
     <div>
-      <h1>Hi, This is the Container page</h1>
+      <h1>Hi, This is the Container Page</h1>
       <hr />
       <MarketingApp />
     </div>
