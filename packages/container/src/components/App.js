3d13de52536823c22950db9ef906379f7e3a79cb
mohammad-23 <mohammad8758@hotmail.com>
2021-06-25 23:37:11 +0530

    Add New Step to workflow

--- packages/container/src/components/App.js
+++ packages/container/src/components/App.js
@@ -5,7 +5,7 @@ import MarketingApp from "./MarketingApp";
 const App = () => {
   return (
     <div>
-      <h1>Hi, This is the Container Page again v2.4</h1>
+      <h1>Hi, This is the Container Page again v3.0</h1>
       <hr />
       <MarketingApp />
     </div>
