791de433d2fef9a3d73f3b11b6576b01baf71f7f
mohammad-23 <mohammad8758@hotmail.com>
2021-06-26 03:40:45 +0530

    Fix workflows

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -7,15 +7,13 @@ import Pricing from "./components/Pricing";
 
 export default () => {
   return (
-    <div>
-      <StylesProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/pricing" component={Pricing} />
-            <Route path="/" component={Landing} />
-          </Switch>
-        </BrowserRouter>
-      </StylesProvider>
-    </div>
+    <StylesProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/pricing" component={Pricing} />
+          <Route path="/" component={Landing} />
+        </Switch>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
