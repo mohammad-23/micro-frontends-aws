e2f4131aaff1c77873f2cd164e15bfa424827027
mohammad-23 <mohammad8758@hotmail.com>
2021-06-26 05:45:34 +0530

    Add Classname Prefixes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StyledProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StyledProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StyledProvider>
     </BrowserRouter>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
