fb1225e6a0f46889583e212e33dbef5b4f4dd53b
mohammad-23 <mohammad8758@hotmail.com>
2021-09-05 08:14:39 +0530

    Add Dashboard App

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,24 +1,38 @@
-import React, { lazy, Suspense, useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
+import { createBrowserHistory } from "history";
 
 import Header from "./components/Header";
 
 const AuthLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
 const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
+// how reactrouterdom works
+// we create this so that we can get access to the history object created by browserRouter
+// which is not possible in this component as it is setup
+// so we use Router and then create our own browserHistory
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    }
+  }, [isSignedIn]);
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header
@@ -36,11 +50,15 @@ export default () => {
                   }}
                 />
               </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardLazy />
+              </Route>
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
